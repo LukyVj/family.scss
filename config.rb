eecfcf9197e2e2162a96318970b5d954ec28ea61
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-05-23 21:26:18 +0200

    Minify css output

--- config.rb
+++ config.rb
@@ -38,7 +38,7 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+   activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
